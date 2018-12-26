fb38fc1741ea62b22ca7bd985bd0eb75dbecbf7b
GuillaumeCisco <guillaumecisco@gmail.com>
2018-12-26 18:20:59 +0100

    Rebase from master

--- src/NavLink.js
+++ src/NavLink.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import type { Connector } from 'react-redux'
 import matchPath from 'rudy-match-path'
